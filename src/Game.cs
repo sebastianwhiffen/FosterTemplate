6398274c50b08341aff6773d33e1f7a5f9012753
sebastian Whiffen <sebastianwhiffen@gmail.com>
2024-07-24 20:06:47 +1000

    movement

--- src/Game.cs
+++ src/Game.cs
@@ -1,33 +1,50 @@
-using Foster.Framework;
-using System.Net.NetworkInformation;
 using System.Numerics;
 
 class Game : Module
 {
-	private readonly IServiceCollection services = new ServiceCollection();
-	private readonly Batcher batch = new();
-	private readonly Texture texture = new Texture(new Image(128, 128, Color.Blue));
+	private static readonly Batcher batch = new();
+	private static readonly Texture texture = new Texture(new Image(128, 128, Color.Blue));
+	private Action UpdateDelegate = () => { };
+	private Action RenderDelegate = () => { };
+
+	private static Entity TestEntity = new Entity { Position = new Vector2(0, 0) };
 
 	public Game()
 	{
+		UpdateDelegate += () => MovementDelegate(TestEntity);
 
+		RenderDelegate += SomeRenderDelegate;
 	}
 
-	public override void Update()
+	private Action<Entity> MovementDelegate = (Entity entity) =>
 	{
-	}
+		var pos = entity.Position;
 
+		if (KeyMap.Left.Any(key => Input.Keyboard.Down((Keys)key))) pos.X -= 4;
+		if (KeyMap.Right.Any(key => Input.Keyboard.Down((Keys)key))) pos.X += 4;
+		if (KeyMap.Forward.Any(key => Input.Keyboard.Down((Keys)key))) pos.Y -= 4;
+		if (KeyMap.Backward.Any(key => Input.Keyboard.Down((Keys)key))) pos.Y += 4;
+		// make this a switch statement
+
+		entity.Position = pos;
+	};
+
+	private Action SomeRenderDelegate = () => batch.Image(texture, TestEntity.Position, Color.Red);
+
+	public override void Update() => UpdateDelegate();
 	public override void Render()
 	{
 		Graphics.Clear(0x44aa77);
 
-		if (KeyMap.Forward.Any(key => Input.Keyboard.Down((Keys)key))) batch.Image(texture, Vector2.Zero, Color.White);
-		
-		else if (KeyMap.Backward.Any(key => Input.Keyboard.Down((Keys)key))) batch.Image(texture, Vector2.Zero, Color.Red);
-
-		// batch.PopMatrix();
+		RenderDelegate();
 
 		batch.Render();
 		batch.Clear();
 	}
+}
+
+public class Entity
+{
+	public Vector2 Position { get; set; }
+
 }
--- src/KeyMap/KeyMap.cs
+++ src/KeyMap/KeyMap.cs
@@ -1,7 +1,8 @@
 public static class KeyMap
 {
 	public static List<int> Forward { get; private set; } = [(int)Keys.W, (int)Keys.Up];
-
-	public static List<int> Backward { get; private set; } = [(int)Keys.W];
+	public static List<int> Backward { get; private set; } = [(int)Keys.S, (int)Keys.Down];
+	public static List<int> Left { get; private set; } = [(int)Keys.A, (int)Keys.Left];
+	public static List<int> Right { get; private set; } = [(int)Keys.D, (int)Keys.Right];
 
 }
